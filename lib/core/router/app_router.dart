13582a503f7eb3195ff8da5c756c1037d31b3f8e
Mark-games <mjcb2022-5740-67472@bicol-u.edu.ph>
2026-03-11 14:33:19 +0800

    login_screen

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -9,6 +9,7 @@ import '../../features/menu/screens/menu_screen.dart';
 import '../../features/events/screens/event_details_screen.dart';
 import '../../features/events/screens/sub_events_details_screen.dart';
 import '../../features/profile/screens/profile_screen.dart';
+import '../../features/auth/screens/login_screen.dart';
 
 
 // Navigator Keys for maintaining state across tabs
@@ -20,7 +21,7 @@ final _shellNavigatorMenuKey = GlobalKey<NavigatorState>(debugLabel: 'menuTab');
 
 final appRouter = GoRouter(
   navigatorKey: _rootNavigatorKey,
-  initialLocation: '/home',
+  initialLocation: '/login',
 
   // TODO: Add redirect logic later for Laravel Sanctum/Token checks
 
@@ -77,10 +78,12 @@ final appRouter = GoRouter(
     ),
 
     // FULL-SCREEN ROUTES (No Bottom Nav Bar here)
+    // LOGIN SCREEN
     GoRoute(
       path: '/login',
+      name: 'login', // Adding a name is good practice for navigation
       parentNavigatorKey: _rootNavigatorKey,
-      builder: (context, state) => const Scaffold(body: Center(child: Text('Login Screen'))),
+      builder: (context, state) => const LoginPage(),
     ),
 
     // PROFILE SCREEN
